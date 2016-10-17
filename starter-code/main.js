5ab05be3ba7cd634019ee9d0d9877dc893989fff
Matthew Cabezon <mcabz27@Matthews-MacBook-Air.local>
2016-10-16 22:04:56 -0400

    edited game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,15 @@ var cardOne = "King";
 var cardTwo = "King";
 var cardThree = "Queen";
 var cardFour = "Queen";
+
+if (cardTwo === cardFour){
+	alert("Sorry, try again.");
+}	else if (cardTwo === cardThree){
+	alert("sorry, try again");
+}	else if (cardOne === cardTwo){
+	alert("You found a match!");
+}	else if (cardOne === cardThree){
+	alert("Sorry, try again");
+}	else if (cardThree === cardFour){
+	alert("You found a match!");
+}	
